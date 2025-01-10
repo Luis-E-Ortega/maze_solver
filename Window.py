454ff4b1f57917a9e5e43b4eb2c3af5ed9aed4a5
Luis-E-Ortega <lorte030@gmail.com>
2025-01-09 20:54:11 -0500

    Added shapes file with Point and Line classes, tested some basic lines in main using new classes

--- Window.py
+++ Window.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, BOTH, Canvas
+from shapes import *
 
 class Window:
     def __init__(self, width=0, height=0):
@@ -18,6 +19,10 @@ class Window:
         # Update and redraw the canvas
         self.__root.update_idletasks()
         self.__root.update()
+    
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+
 
     def wait_for_close(self):
         # Set the running state to True and start the loop
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from Window import Window
+from shapes import Point, Line
 
 def main():
     win = Window(800, 600)
+
+    point1 = Point(100, 100)
+    point2 = Point(300, 300)
+
+    my_line = Line(point1, point2)
+
+    win.draw_line(my_line, "green")
+
     win.wait_for_close()
 
 if __name__ == "__main__":
